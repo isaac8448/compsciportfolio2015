9f1cf6349191be8ac25d8c5826143a88bb04ad84
isaac8448 <isanchez.sp@mysummitps.com>
2016-01-27 21:01:03 +0000

    update

--- randomGenerator.js
+++ randomGenerator.js
@@ -1,31 +1,60 @@
 // JavaScript File
 var button = document.getElementById("button");
-var number = 0;
-var number2 = 0;
-var color;
-var color2;
-var colors = ["white", "black", "red", "blue", "green", "yellow", "teal", "purple", "orange", "indigo", "aqua", "brown", "crimson", "grey","chartreuse"];
-function changeBackground(){
-    document.getElementById("body").style.backgroundColor = color;
+var x = 0;
+var sound = new Audio('http://www.flashkit.com/imagesvr_ce/flashkit/soundfx/Interfaces/Pops/Hit_Mark-Metallic-9040/Hit_Mark-Metallic-9040_hifi.mp3')
+function randomizePage(){
+    document.getElementById("body").style.backgroundColor = randomColor();
+document.body.appendChild( randomElement()   );
+document.body.appendChild(randomElement2()   );
+
+}
+button.addEventListener("click", randomizePage);
+
+function randomElement(){
+    var tempL = document.createElement("div");
+    tempL.innerHTML = randomWord();
+    tempL.style.backgroundColor = randomColor();
+    tempL.style.color = randomColor();
+    tempL.style.position = "absolute"
+    tempL.style.top = Math.floor((Math.random() * document.body.scrollHeight ) + 1) + "px";
+    tempL.style.left = Math.floor((Math.random() * document.body.scrollWidth ) + 1) + "px";
+    sound.play();
+    return tempL;
 }
 
-button.addEventListener("click", function(){
-    changeBackground();
-    changeBorder();
-})
-
-function changeBorder(){
-    document.getElementById("body").style.color = color2;
+function randomElement2(){
+    var tempL2 = document.createElement("div");
+    tempL2.innerHTML = randomNumber();
+    sound.play();
+    tempL2.style.backgroundColor = randomColor();
+    tempL2.style.color = randomColor();
+    tempL2.style.position = "absolute"
+    tempL2.style.top = Math.floor((Math.random() * document.body.scrollHeight ) + 1) + "px";
+    tempL2.style.left = Math.floor((Math.random() * document.body.scrollWidth ) + 1) + "px";
+    return tempL2;
 }
 
+function randomColor(){
+    var color;
+    sound.play();
+    var colors = ["white", "black", "red", "blue", "green", "yellow", "teal", "purple", "orange", "indigo", "aqua", "brown", "crimson", "grey","chartreuse"]
+    x =  Math.floor(( Math.random()* colors.length));
+    color = colors[x];  
+    return color;
+}
 
-function randomNumber()
-{
-    number =  Math.floor((Math.random()*14));
-  color = colors[number];
+function randomWord(){
+    var word;
+    var words = ["WOW", "M8", "REKT", "MEXICAN AMERICANS", "YOLO", "PEPE", "PAPI", "WORDS", "PONG", "DEWITTOS", "MTN DEW", "MLG", "SWEG", "NICE","COOL"]
+    x =  Math.floor(( Math.random()* words.length));
+    word = words[x];  
+    return word;
 }
-function randomNumber2()
-{
-    number2 =  Math.floor((Math.random()*14));
-  color2 = colors[number2];
+function randomNumber(){
+    var number;
+    var numbers = ["84", "69", "96", "21", "48", "53", "650", "88", "6", "10", "7", "1", "94", "72","66"]
+    x =  Math.floor(( Math.random()* numbers.length));
+    number = numbers[x];  
+    return number;
 }
+
